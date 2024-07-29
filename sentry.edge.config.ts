e7fd19282d08710f4569e92ad6bafa7c6d7d92af
Om Singhan <omsinghan25@gmail.com>
2024-07-28 20:24:41 -0400

    add sentry for scaling and tracking errors in the application

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://91a7a95bb8b0436743163b7eef308a86@o4507682176040960.ingest.us.sentry.io/4507682178072576",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
