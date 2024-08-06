d4ccaf020ce57df00a9263cc9145f062905683c1
Om Singhan <omsinghan25@gmail.com>
2024-08-05 23:15:31 -0400

    deploy

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,23 +9,23 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn =await getLoggedInUser();
-  console.log('nouser',loggedIn)
+  const loggedIn = await getLoggedInUser();
+
   if(!loggedIn) redirect('/sign-in')
 
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn} />
-      <div className="flex flex-col">
-        <div className="root-layout">
-          <Image src  = "/icons/logo.svg" width={30} height={30} alt="menu Logo" />
-          <div>
-            <MobileNav user={loggedIn}/>
-          </div>
 
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
         </div>
-      </div>
         {children}
+      </div>
     </main>
   );
 }
